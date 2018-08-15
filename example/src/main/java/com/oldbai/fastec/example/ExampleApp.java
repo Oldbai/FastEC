2b18f1e48ec22d48744d878e6e335d674c3e3dd7
oldbai <baiguoyong@gmail.com>
2018-08-15 08:34:20 +0800

    1、框架基本搭建

---
+++ example/src/main/java/com/oldbai/fastec/example/ExampleApp.java
@@ -0,0 +1,16 @@
+package com.oldbai.fastec.example;
+
+import android.app.Application;
+
+import com.oldbai.latte_core.app.Latte;
+
+public class ExampleApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Latte.init(this)
+                .withApiHost("http://127.0.0.1/")
+                .configure();
+    }
+}
