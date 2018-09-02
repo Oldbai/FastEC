41b401c5f4f18c0037ee1df5eb794c7e131fc5d1
oldbai <baiguoyong@gmail.com>
2018-09-02 17:34:07 +0800

    主页布局初步完成

---
+++ latte-core/src/main/java/com/oldbai/latte_core/ui/banner/HolderCreator.java
@@ -0,0 +1,11 @@
+package com.oldbai.latte_core.ui.banner;
+
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+
+public class HolderCreator implements CBViewHolderCreator<ImageHolder>{
+
+    @Override
+    public ImageHolder createHolder() {
+        return new ImageHolder();
+    }
+}
--- latte-core/src/main/java/com/oldbai/latte_core/ui/refresh/RefreshHandler.java
+++ latte-core/src/main/java/com/oldbai/latte_core/ui/refresh/RefreshHandler.java
@@ -1,19 +1,43 @@
 package com.oldbai.latte_core.ui.refresh;
 
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.widget.Toast;
+import android.support.v7.widget.RecyclerView;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.oldbai.latte_core.app.Latte;
 import com.oldbai.latte_core.net.RestClient;
 import com.oldbai.latte_core.net.callback.ISuccess;
+import com.oldbai.latte_core.ui.recycler.DataConverter;
+import com.oldbai.latte_core.ui.recycler.MultipleRecyclerAdapter;
 
-public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener{
+public class RefreshHandler implements
+        SwipeRefreshLayout.OnRefreshListener,
+        BaseQuickAdapter.RequestLoadMoreListener{
 
     private final SwipeRefreshLayout REFRESH_LAYOUT;
+    private final PagingBean BEAN;
+    private final RecyclerView RECYCLERVIEW;
+    private MultipleRecyclerAdapter mAdapter = null;
+    private final DataConverter CONVERTER;
 
-    public RefreshHandler(SwipeRefreshLayout refresh_layout) {
-        REFRESH_LAYOUT = refresh_layout;
+
+    private RefreshHandler(SwipeRefreshLayout swipeRefreshLayout,
+                           RecyclerView recyclerView,
+                           DataConverter converter,
+                           PagingBean bean) {
+        REFRESH_LAYOUT = swipeRefreshLayout;
+        RECYCLERVIEW = recyclerView;
+        CONVERTER = converter;
+        BEAN = bean;
         REFRESH_LAYOUT.setOnRefreshListener(this);
+    }
+
+    public static RefreshHandler create(SwipeRefreshLayout swipeRefreshLayout,
+                                        RecyclerView recyclerView,
+                                        DataConverter converter) {
+        return new RefreshHandler(swipeRefreshLayout, recyclerView, converter, new PagingBean());
     }
 
 
@@ -25,20 +49,32 @@ public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener{
             public void run() {
                 REFRESH_LAYOUT.setRefreshing(false);
             }
-        },2000);
+        }, 2000);
     }
 
-    public void firstPage(String url){
+    public void firstPage(String url) {
+        BEAN.setDelayed(1000);
         RestClient.builder()
                 .url(url)
                 .success(new ISuccess() {
                     @Override
                     public void onSuccess(String response) {
-                        Toast.makeText(Latte.getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+                        final JSONObject object = JSON.parseObject(response);
+                        BEAN.setTotle(object.getInteger("total"))
+                                .setPageSize(object.getInteger("page_size"));
+                        //设置Adapter
+                        mAdapter = MultipleRecyclerAdapter.create(CONVERTER.setJsonData(response));
+                        mAdapter.setOnLoadMoreListener(RefreshHandler.this,RECYCLERVIEW);
+                        RECYCLERVIEW.setAdapter(mAdapter);
+                        BEAN.addIndex();
                     }
                 })
                 .build()
                 .get();
     }
 
+    @Override
+    public void onLoadMoreRequested() {
+
+    }
 }
